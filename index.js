24c6c4bd7b3181860f222a00990b759ae866ebe2
Nick Ruiz <nick.ruiz1922@gmail.com>
2022-09-26 17:20:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let homeScore = document.getElementById("home")
+console.log(homeScore)
+let guestScore = document.getElementById("guest")
+
+
+let homeResult = 0
+let guestResult = 0
+console.log(score)
+
+function add1Point() {
+    homeResult += 1
+    homeScore.textContent = homeResult
+}
+
+function add2Points() {
+    homeResult += 2
+    homeScore.textContent = homeResult
+}
+
+function add3Points() {
+    homeResult += 3
+    homeScore.textContent = homeResult
+}
+
+function guest1Point() {
+    guestResult += 1
+    guestScore.textContent = guestResult
+}
+
+function guest2Points() {
+    guestResult += 2
+    guestScore.textContent = guestResult
+}
+
+function guest3Points() {
+    guestResult += 3
+    guestScore.textContent = guestResult
+}
+
+ function reset() {
+    homeScore.textContent = 0
+    guestScore.textContent = 0
+}
